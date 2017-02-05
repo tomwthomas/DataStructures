2f81b44e4e8b83271d4299e9ccc3df89522ed6fb
twt <tomwthomas@gmail.com>
2017-02-05 15:03:05 -0800

    checking in first pass of delete for HTDS

--- src/HashTableDataStructure.java
+++ src/HashTableDataStructure.java
@@ -44,19 +44,59 @@ public class HashTableDataStructure {
         // get the key index based on the key
         int index = getKeyIndex(key);
 
-        System.out.println("looking for index:" + index);
-
-        // check the hash location in the phoneEntries object so see if there is an entry here or not
+         // check the hash location in the phoneEntries object so see if there is an entry here or not
         if(phoneEntries[index] != null) { // found a potential match, check and if so remove it, otherwise traverse chain until end or a match is detected
             System.out.println("found a potential key match");
             // check to see if the current node is the key I'm looking for
             System.out.println("got this key:" + phoneEntries[index].getKey() + " and was looking for:" + key);
-            if(phoneEntries[index].getKey().equals(key)) {
+
+            // check to see if the current entry is the one we are looking for
+            Node currentNode = phoneEntries[index];
+            Node nextNode = currentNode.getNextNode();
+            if(currentNode.getKey().equals(key)) { // we found the entry we are looking for
+                // check to see if this entry has a chain and if so, move it left, otherwise null out this entry
+                if(nextNode != null)
+                    phoneEntries[index] = nextNode;
+                else
+                    phoneEntries[index] = null;
+
+                System.out.println("next node is now set after a delete in main if statement...");
+            }
+            else { // we have an entry but not a key match, go through the chain if there is one
+                // loop over the chain until we find a match
+                // when we find a match, null it out, if there is a chain, make the next entry the tail of the tail
+                // potential use cases:
+                // miss, hit, something not null (at some length) - point miss at something not null
+                // miss, hit, null - point miss at null
+                // miss, null - no match found, do nothing
+                // hit, null - taken care of above in the original if statement
+                // null - no match found, do nothing
+
+                // since it is not the first position all we really care about is the nextNode pointer irregardless of the array itself
+                while(nextNode != null) {
+                    // check to see if the next node has the value we want to delete
+                    if(nextNode.getKey().equals(key)) { // we found the entry we are looking for
+                        // since the next node has the value we want to delete assign it's next node to the current nodes next node and essentially drop the next node from the chain
+                        currentNode.setNextNode(nextNode.getNextNode()); // this works because we don't care what the value is - null or otherwise
+                        break; // since we deleted the requested entry break out of the loop
+                    }
+                    else // we did not yet find the entry we are looking for so move to the next link in the chain if it exists
+                        nextNode = nextNode.getNextNode(); // will be null if there are no more links in the chain
+                }
+
+                System.out.println("in else statement, nextNode now set to:" + nextNode.getKey());
+            }
+
+
+
+
+/*            if(phoneEntries[index].getKey().equals(key)) {
                 System.out.println("found a matching key");
                 if(phoneEntries[index].getNextNode() != null) {
                     System.out.println("this entry has a chain after it");
+                    phoneEntries[index] = phoneEntries[index].getNextNode();  // since we have a chain after this entry, move it left one notch
                 }
-                else {
+                else { // this entry has no chain so we are clear to delete it
                     phoneEntries[index] = null;
                     System.out.println("removed entry - had no chain");
                 }
@@ -71,24 +111,23 @@ public class HashTableDataStructure {
                             System.out.println("this entry has a chain after it");
                         }
                         else {
-                            phoneEntries[index] = null;
+                              phoneEntries[index] = null;
                             System.out.println("removed entry - had no chain");
                         }
                     }
                 }
-            }
-            // if so, check if it has a chain following it
-                // if there is a chain, move the first entry in the chain to the head of the list
-                // if there is no chain then simply null this entry and null the node itself so as to free memory
-            // else, we found an entry but not the right key so check the next entry in the chain
-                // potential recursion based on the if above at this point!!!
+            }*/
 
         }
-        else { // since we don't yet have an entry here, then add this new entry as a new node
-           // System.out.println("found an empty spot!");
-            //phoneEntries[index] = newNode;
-            //System.out.println("added new bucket:" + phoneEntries[index].getValue().getFirstName());
+        else { // no match was found at the expected hash location so do nothing
+           System.out.println("no match found");
         }
+    }
+
+    public String lookupEntry() {
+
+
+        return null;
     }
 
     private String generateKey(String firstName, String lastName) {
@@ -106,5 +145,26 @@ public class HashTableDataStructure {
         System.out.println("modHash:" + index);
 
         return index;
+    }
+
+    private Node adjustChainForDelete(Node currentNode, String desiredKey) { // this may become my lookup function!!
+
+        Node nextNode = currentNode.getNextNode(); // defaults to null if there are no entries following this one
+        if( nextNode != null) {
+            if(nextNode.getKey().equals(desiredKey)) {
+                System.out.println("found a matching key");
+                if(nextNode != null) {
+                    System.out.println("this entry has a chain after it");
+                    // phoneEntries[index] = phoneEntries[index].getNextNode();  // since we have a chain after this entry, move it left one notch
+                }
+                else {
+                    // we are at the correct entry
+                    // phoneEntries[index] = null;
+                    System.out.println("removed entry - had no chain");
+                }
+            }
+        }
+
+        return nextNode;
     }
 }
