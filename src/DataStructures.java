fddc8ae54ef843919d9c4809b748122ec46de6c1
twt <tomwthomas@gmail.com>
2017-02-04 18:00:29 -0800

    Stubbing out test cases

--- src/DataStructures.java
+++ src/DataStructures.java
@@ -4,5 +4,62 @@ public class DataStructures
     {
 
         System.out.println("hello world!");
+
+        // TWT:  Begin test cases for Hash Table Data Structure (HTDS)
+//        Insert Bob Smith 555-235-1111 bsmith@somewhere.com
+//        Insert Jane Williams 555-235-1112 jw@something.com
+//        Insert Mohammed al-Salam 555-235-1113 mas@someplace.com
+//        Insert Pat Jones 555-235-1114 pjones@homesweethome.com
+//        Insert Billy Kidd 555-235-1115 billy_the_kid@nowhere.com
+//        Insert H. Houdini 555-235-1116 houdini@noplace.com
+//        Insert Jack Jones 555-235-1117 jjones@hill.com
+//        Insert Jill Jones 555-235-1118 jillj@hill.com
+//        Insert John Doe 555-235-1119 jdoe@somedomain.com
+//        Insert Jane Doe 555-235-1120 jdoe@somedomain.com
+//        Lookup Pat Jones
+//        Lookup Billy Kidd
+//        Delete John Doe
+//        Insert Test Case 555-235-1121 Test_Case@testcase.com
+//        Insert Nadezhda Kanachekhovskaya 555-235-1122 dr.nadezhda.kanacheckovskaya@somehospital.moscow.ci.ru
+//        Insert Jo Wu 555-235-1123 wu@h.com
+//        Insert Millard Fillmore 555-235-1124 millard@theactualwhitehouse.us
+//        Insert Bob vanDyke 555-235-1125 vandyke@nodomain.com
+//        Insert Upside Down 555-235-1126 upsidedown@rightsideup.com
+//        Lookup Jack Jones
+//        Lookup Nadezhda Kanachekhovskaya
+//        Delete Jill Jones
+//        Delete John Doe
+//        Lookup Jill Jones (What should happen if the “lookup” message doesn’t find the entry?)
+//        Lookup John Doe
+
+        // TWT:  Begin test cases for Tree Data Structure (TDS)
+//        Insert Bob Smith 555-235-1111 bsmith@somewhere.com
+//        Insert Jane Williams 555-235-1112 jw@something.com
+//        Insert Mohammed al-Salam 555-235-1113 mas@someplace.com
+//        Insert Pat Jones 555-235-1114 pjones@homesweethome.com
+//        Insert Billy Kidd 555-235-1115 billy_the_kid@nowhere.com
+//        Insert H. Houdini 555-235-1116 houdini@noplace.com
+//        Insert Jack Jones 555-235-1117 jjones@hill.com
+//        Insert Jill Jones 555-235-1118 jillj@hill.com
+//        Insert John Doe 555-235-1119 jdoe@somedomain.com
+//        Insert Jane Doe 555-235-1120 jdoe@somedomain.com
+//        Lookup Pat Jones
+//        Lookup Billy Kidd
+//        Delete John Doe
+//        Insert Test Case 555-235-1121 Test_Case@testcase.com
+//        Insert Nadezhda Kanachekhovskaya 555-235-1122 dr.nadezhda.kanacheckovskaya@somehospital.moscow.ci.ru
+//        Insert Jo Wu 555-235-1123 wu@h.com
+//        Insert Millard Fillmore 555-235-1124 millard@theactualwhitehouse.us
+//        Insert Bob vanDyke 555-235-1125 vandyke@nodomain.com
+//        Insert Upside Down 555-235-1126 upsidedown@rightsideup.com
+//        Lookup Jack Jones
+//        Lookup Nadezhda Kanachekhovskaya
+//        Delete Jill Jones
+//        Delete John Doe
+//        Lookup Jill Jones (What should happen if the “lookup” message doesn’t find the entry?)
+//        Lookup John Doe
+
+        System.out.println("good bye world!");
+
     }
 }
